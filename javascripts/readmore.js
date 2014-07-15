6a124d55e3551c43d4e2ce63c7eaea43e3f20ea3
Lukas Schwab <lukas.schwab@gmail.com>
2014-07-14 23:34:40 -0700

    added more connection links, added my copy, added stolen js from colbys site

---
+++ javascripts/readmore.js
@@ -0,0 +1,2 @@
+// "{read less}" doesn't appear in-line
+function toggleExtra(){var e=document.getElementsByClassName("extra")[0],a=document.getElementById("more");"block"==e.style.display?(e.style.display="none",a.innerHTML="{read more}"):(e.style.display="block",e.className="extra animated fadeIn",a.innerHTML="{read less}")}
